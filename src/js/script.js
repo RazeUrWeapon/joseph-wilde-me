5eda54b2542ae6316bf1df15386fe500e851bfa6
Joseph Wilde <jwilde19@gmail.com>
2022-01-11 14:26:25 -0500

    Write bio text for hero section and integrate typewriter effect

--- src/js/script.js
+++ src/js/script.js
@@ -35,3 +35,56 @@ hamburgerMenu.addEventListener('click', () => {
 mobileMenu.addEventListener('click', () => {
     if(nav.classList.contains('open')) {closeHamburgerMenu()}
 });
+
+// Typewriter effect in hero section
+const typewriterText = document.querySelector('.typewriter-text');
+const phrases = [
+    'being a dog dad. ',
+    'learning about Web3 technologies. ',
+    'DJing for fun. ',
+    'playing games with friends. '
+]
+
+let i = 0;
+let j = 0;
+let currentPhrase = [];
+let isDeleting = false;
+let isEnd = false;
+let typewriterTime = 90;
+
+function loopTypewriterText() {
+    isEnd = false;
+
+    typewriterText.innerHTML = currentPhrase.join('');
+
+    if (i < phrases.length) {
+        if (!isDeleting && j <= phrases[i].length) {
+            currentPhrase.push(phrases[i][j]);
+             j++;
+        }
+
+        if (isDeleting && j <= phrases[i].length) {
+            typewriterTime = 90;
+            currentPhrase.pop(phrases[i][j]);
+            j--;
+        }
+
+        if (j == phrases[i].length) {
+            typewriterTime = 1250;
+            setTimeout(isDeleting = true, 1250);
+        }
+
+        if (isDeleting && j === 0) {
+            currentPhrase = [];
+            isDeleting = false;
+            i++;
+
+            if (i == phrases.length) {
+                i = 0;
+            }
+        }
+    }
+    setTimeout(loopTypewriterText, typewriterTime);
+}
+
+loopTypewriterText();
