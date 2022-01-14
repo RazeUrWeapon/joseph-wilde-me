83eac23e15cf30aadb084bd5b5875ef966052eec
Joseph Wilde <jwilde19@gmail.com>
2022-01-14 15:17:51 -0500

    Finish design and layout for entire main page

--- src/js/script.js
+++ src/js/script.js
@@ -117,3 +117,59 @@ workEmail.addEventListener('touchend', returnEmailText);
 formButton.addEventListener('submit', () => {
     contactForm.innerHTML = `<h2> I'll get back to you as soon as possible :)</h2>`;
 });
+
+// Hide header on scroll down and show on scroll up
+// ** https://codingreflections.com/hide-header-on-scroll-down/ **
+(function(){
+
+    let doc = document.documentElement;
+    let w = window;
+  
+    let prevScroll = w.scrollY || doc.scrollTop;
+    let curScroll;
+    let direction = 0;
+    let prevDirection = 0;
+
+    const header = document.querySelector('header');
+  
+    let checkScroll = function() {
+  
+      /*
+      ** Find the direction of scroll
+      ** 0 - initial, 1 - up, 2 - down
+      */
+  
+      curScroll = w.scrollY || doc.scrollTop;
+      if (curScroll > prevScroll) { 
+        //scrolled up
+        direction = 2;
+      }
+      else if (curScroll < prevScroll) { 
+        //scrolled down
+        direction = 1;
+      }
+  
+      if (direction !== prevDirection) {
+        toggleHeader(direction, curScroll);
+      }
+  
+      prevScroll = curScroll;
+    };
+  
+    var toggleHeader = function(direction, curScroll) {
+      if (direction === 2 && curScroll > 58.5) { 
+  
+        //replace 52 with the height of your header in px
+  
+        header.classList.add('hide');
+        prevDirection = direction;
+      }
+      else if (direction === 1) {
+        header.classList.remove('hide');
+        prevDirection = direction;
+      }
+    };
+  
+    window.addEventListener('scroll', checkScroll);
+  
+  })();
