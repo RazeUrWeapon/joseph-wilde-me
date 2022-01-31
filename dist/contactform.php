66e31786559928ea78c144a6de116291a8dfc528
Joseph Wilde <jwilde19@gmail.com>
2022-01-31 14:14:04 -0500

    Add subject to contactform.php

--- dist/contactform.php
+++ dist/contactform.php
@@ -4,7 +4,7 @@ if (isset($_POST['submit'])) {
     $name = $_POST['name'];
     $mailFrom = $_POST['email'];
     $message = $_POST['message'];
-
+    $subject = "Inquiry from Portfolio Website";
     $mailTo = "joe@josephwilde.me";
     $headers = "From: ".$mailFrom;
     $txt = "You have received an e-mail from ".$name.".\n\n".$message;
